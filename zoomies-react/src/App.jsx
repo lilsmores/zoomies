32318051e9e9f886c4696a7fe7c13741e9580f1f
Lili <liannagraham@MacBook.local>
2025-07-25 23:38:00 -0400

    Fix routing for GitHub Pages and update title

--- zoomies-react/src/App.jsx
+++ zoomies-react/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ZoomiesHeader from './components/ZoomiesHeader';
 import AmbassadorProfilePage from './pages/AmbassadorProfilePage';
 import AmbassadorProfilePageCopy from './pages/AmbassadorProfilePageCopy';
